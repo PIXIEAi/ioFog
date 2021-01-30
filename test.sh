bb88cc720bbe851578baa5a89a77355b0c228c31
Hanie Poursina <hanieh.poursina@egmail.com>
2021-01-30 14:23:56 -0800

    to download iofog script

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+wget https://github.com/eclipse-iofog/iofogctl/archive/v2.0.3.tar.gz
+tar -xzf v2.0.3.tar.gz
+cd iofogctl-2.0.3
+wget https://golang.org/dl/go1.15.7.linux-arm64.tar.gz
+sudo tar -C /usr/local/ -xzf go1.15.7.linux-arm64.tar.gz
+PATH=$PATH:/usr/local/go/bin
+GOPATH=$HOME/go
+source ~/.profile
+sudo apt install golang-rice
+wget https://github.com/PIXIEAi/ioFog/blob/main/Makefile
+make build
+cd bin
+sudo cp iofogctl /usr/local/bin/.
+
