d8a8cb17045bb7720480c67b7cc45bb99e5a0300
PIXIEAi <pixieteamai@gmail.com>
2021-02-01 13:46:20 +0330

    updated

--- iofog-32/iofog-installation-32.sh
+++ iofog-32/iofog-installation-32.sh
@@ -2,13 +2,13 @@
 
 wget https://github.com/eclipse-iofog/iofogctl/archive/v2.0.3.tar.gz
 tar -xzf v2.0.3.tar.gz
-cd iofogctl-2.0.3
 sudo tar -C /usr/local/ -xzf go1.15.7.linux-armv6l.tar.gz
 PATH=$PATH:/usr/local/go/bin
 GOPATH=$HOME/go
 source ~/.profile
 sudo apt install golang-rice
-wget https://github.com/PIXIEAi/ioFog/blob/main/Makefile
+mv Makefile iofogctl-2.0.3/
+cd iofogctl-2.0.3
 make build
 cd bin
 sudo cp iofogctl /usr/local/bin/.
